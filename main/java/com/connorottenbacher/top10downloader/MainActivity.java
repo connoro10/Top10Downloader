73ca62d6bbafc78ed9ce5f45b4fb5fe7b7e7f6f3
connor <connorotte@gmail.com>
2017-02-25 01:59:37 -0700

    began work on asynchronous downloader

---
+++ main/java/com/connorottenbacher/top10downloader/MainActivity.java
@@ -0,0 +1,95 @@
+package com.connorottenbacher.top10downloader;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting async" );
+        DownloadData d = new DownloadData();
+        d.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+        Log.d(TAG, "oncreate done ");
+
+
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String>
+    {
+        private static final String TAG = "DownloadData";
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is " + s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doInBackground: starts with " + strings[0]);
+            String rss = downloadXML(strings[0]);
+            if(rss == null)
+            {
+                Log.e(TAG, "error downloading");
+            }
+            return rss;
+        }
+        private String downloadXML(String urlPath)
+        {
+            StringBuilder xmlResult = new StringBuilder();
+            try
+            {
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d(TAG, "response code was: " + response);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                int charsRead;
+                char[] inputBuffer = new char[500];
+                while(true)
+                {
+                    charsRead = reader.read(inputBuffer);
+                    if(charsRead < 0)
+                    {
+                        break;
+                    }
+                    if(charsRead > 0)
+                    {
+                        xmlResult.append(String.copyValueOf(inputBuffer, 0, charsRead));
+                    }
+                }
+                reader.close();
+                return xmlResult.toString();
+            }
+            catch(MalformedURLException e)
+            {
+                Log.e(TAG, "invalid URL ");
+            }
+            catch(IOException e)
+            {
+                Log.e(TAG, "what have you done IO exception thrown ");
+            }
+            catch(SecurityException e)
+            {
+                Log.e(TAG, "security you need permiso " + e.getMessage());
+            }
+            return null;
+        }
+    }
+}
+
+
+
